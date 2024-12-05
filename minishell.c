be6a018849e98549229084a76d08254538164765
rodriCS7 <r.chivo.2020@alumnos.urjc.es>
2024-12-05 18:26:04 +0100

    redirección de entrada de fichero y de salida a fichero para 1 y 2 mandatos

--- minishell.c
+++ minishell.c
@@ -3,64 +3,134 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 #include "parser.h"
 #include <signal.h>
 
-int main()
-{
+int main() {
 
+	// Ignorar la señal SIGINT en el proceso padre
 	signal(SIGINT, SIG_IGN);
 
-	while (1)
-	{
+	while (1) {
 
 		printf("msh> ");
 		fflush(stdout);
 
 		char buff[1024];
-		if (fgets(buff, sizeof(buff), stdin) == 0)
-		{
+		if (fgets(buff, sizeof(buff), stdin) == 0) {
 			break;
 		}
 
+		// Tokenizamos la línea ingresada
 		tline *line;
 		line = tokenize(buff);
 
-		if (line->ncommands == 1)
-		{
-			pid_t pid = fork();
-			if (pid == 0) {
-				signal(SIGINT, SIG_DFL);
+		int input_fd = -1; // Descriptor de fichero para redirección de entrada
+		int output_fd = -1; // Descriptor de fichero para redirección de salida
 
-				if (execvp(line->commands[0].filename, line->commands[0].argv) == -1) {
-					fprintf(stderr, "Error al ejecutar el mandato");
+		// Redirigir la entrada estandar
+		if (line->redirect_input != NULL) {
+			input_fd = open(line->redirect_input, O_RDONLY);
+			if (input_fd == -1) {
+				fprintf(stderr, "Error al abrir el archivo de entrada");
+			} 
+		}
+
+		// Redirigir la salida estándar
+		if (line->redirect_output != NULL) {
+			output_fd = open(line->redirect_output, O_WRONLY | O_CREAT | O_TRUNC);
+			if (output_fd == -1) {
+				fprintf(stderr, "Error al abrir o crear el fichero de salida");
+			}
+		}
+
+		if (line->ncommands == 1) {
+			pid_t pid = fork();
+
+			// Si se ha producido un error al hacer el fork
+			if (pid == -1) {
+				if (input_fd != -1) {
+					close(input_fd);
+				}
+				if (output_fd != -1) {
+					close(output_fd);
 				}
 			}
 
+			// Si somos el hijo
+			if (pid == 0) {
+
+				// Restauramos el comportamiento de la señal SIGINT
+				signal(SIGINT, SIG_DFL); 
+
+				// redirigimos la entrada estándar
+				if (input_fd != -1) {
+					dup2(input_fd, STDIN_FILENO);
+					close(input_fd);
+				}
+				// redirigimos la salida estándar
+				if (output_fd != -1) {
+					dup2(output_fd, STDOUT_FILENO);
+					close(output_fd);
+				}
+
+				// Ejecutamos el comando
+				execvp(line->commands[0].filename, line->commands[0].argv);
+				
+				// En caso de error
+				fprintf(stderr, "Error al ejecutar el mandato");
+				return -1;
+				
+			}
+
+			// Si somos el padre
+			if (input_fd != -1) {
+				close(input_fd);
+			}
+			if (output_fd != -1) {
+				close(output_fd);
+			}
 			waitpid(pid, NULL, 0);
 		}
-		else
-		{
+		else if (line->ncommands == 2) {
 
 			int pipefd[2];
 
-			if (pipe(pipefd) == -1)
-			{
+			// Si se produce un error al crear el pipe
+			if (pipe(pipefd) == -1) {
 				fprintf(stderr, "Error al crear el pipe\n");
+				if (input_fd != -1) {
+					close(input_fd);
+				}
+				if (output_fd != -1) {
+					close(output_fd);
+				}
 			}
 
 			pid_t pid1 = fork();
-			if (pid1 == -1)
-			{
+			if (pid1 == -1){
 				fprintf(stderr, "Error al hacer el fork para el primer hijo\n");
+				close(pipefd[0]);
+                close(pipefd[1]);
+                if (input_fd != -1) close(input_fd);
+                if (output_fd != -1) close(output_fd);
 			}
 
 			// Somos el pimer hijo
-			if (pid1 == 0)
-			{
+			if (pid1 == 0) {
+
+				// Restauramos el funcionamiento de la señal SIGINT
 				signal(SIGINT, SIG_DFL);
+
+				// Redirigmos la entrada estandar
+				if (input_fd != -1) {
+					dup2(input_fd, STDIN_FILENO);
+					close(input_fd);
+				}
+
 				close(pipefd[0]);				// Cerramos el extremo de lectura
-				dup2(pipefd[1], STDOUT_FILENO); // Redirigimos stdout al extremos de escritura del pipe
+				dup2(pipefd[1], STDOUT_FILENO); // Redirigimos stdout al extremo de escritura del pipe
 				close(pipefd[1]);				// Cerramos el extremo de escritura
 
 				tcommand *cmd1 = &line->commands[0];
@@ -69,17 +139,27 @@ int main()
 			}
 
 			pid_t pid2 = fork();
-			if (pid2 == -1)
-			{
+			if (pid2 == -1) {
 				fprintf(stderr, "Error al hacer el fork para el segundo hijo\n");
+				close(pipefd[0]);
+                close(pipefd[1]);
+                if (input_fd != -1) close(input_fd);
+                if (output_fd != -1) close(output_fd);
 			}
 
 			// Somos el segundo hijo
-			if (pid2 == 0)
-			{
+			if (pid2 == 0) {
+
+				// Restauramos el funcionamiento de la señal SIGINT
 				signal(SIGINT, SIG_DFL);
-				close(pipefd[1]);
-				dup2(pipefd[0], STDIN_FILENO);
+
+				if (output_fd != -1) {
+					dup2(output_fd, STDOUT_FILENO);
+					close(output_fd);
+				}
+
+				close(pipefd[1]); // Cerramos el extremo de escritura del pipe
+				dup2(pipefd[0], STDIN_FILENO); // Redirigimos la entrada estándar al pipe
 				close(pipefd[0]);
 
 				tcommand *cmd2 = &line->commands[1];
@@ -90,6 +170,13 @@ int main()
 			// Código del padre
 			close(pipefd[0]);
 			close(pipefd[1]);
+			if (input_fd != -1) {
+				close(input_fd);
+			}
+			if (output_fd != -1) {
+				close(output_fd);
+			}
+
 			waitpid(pid1, NULL, 0);
 			waitpid(pid2, NULL, 0);
 		}
