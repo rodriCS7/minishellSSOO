ff037e35db3d4623adda3d5f606462195100eb90
Samurott23 <ivanricoinstituto@gmail.com>
2024-12-18 17:58:25 +0100

    Update probandocondinamica2.c

--- probandocondinamica2.c
+++ probandocondinamica2.c
@@ -75,102 +75,114 @@ int main() {
             break;
         }
 
-        tline *line = tokenize(buff);
-        if (line == NULL) continue;
-
-        if (strcmp(buff, "cd\n") == 0 || strncmp(buff, "cd ", 3) == 0) {
-            char *dir = strtok(buff + 3, "\n");
-            if (dir == NULL) dir = getenv("HOME");
-            if (chdir(dir) == -1) fprintf(stderr, "Error al cambiar de directorio\n");
-        } else if (strcmp(buff, "jobs\n") == 0) {
-            for (int i = 0; i < job_count; i++) {
-                if (jobs[i].active) {
-                    printf("[%d]+ %-7s %s\n", jobs[i].id, jobs[i].status, jobs[i].command);
-                }
-            }
-        } else if (strncmp(buff, "fg", 2) == 0) {
-            char *index = strtok(buff + 3, "\n");
-            fg(index);
-        } else {
-            int input_fd = -1, output_fd = -1, error_fd = -1;
-
-            // **Redirección de entrada**
-            if (line->redirect_input) {
-                input_fd = open(line->redirect_input, O_RDONLY);
-                if (input_fd == -1) {
-                    fprintf(stderr, "Error al abrir el archivo de entrada: %s\n", line->redirect_input);
-                    continue;
-                }
+        // Dividir la línea en comandos separados por ';'
+        char *command = strtok(buff, ";\n");
+        while (command != NULL) {
+            tline *line = tokenize(command);
+            if (line == NULL) {
+                command = strtok(NULL, ";\n");
+                continue;
             }
 
-            // **Redirección de salida**
-            if (line->redirect_output) {
-                output_fd = open(line->redirect_output, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-                if (output_fd == -1) {
-                    fprintf(stderr, "Error al abrir el archivo de salida: %s\n", line->redirect_output);
-                    if (input_fd != -1) close(input_fd);
-                    continue;
-                }
-            }
-
-            // **Redirección de error**
-            if (line->redirect_error) {
-                error_fd = open(line->redirect_error, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-                if (error_fd == -1) {
-                    fprintf(stderr, "Error al abrir el archivo de error: %s\n", line->redirect_error);
-                    if (input_fd != -1) close(input_fd);
-                    if (output_fd != -1) close(output_fd);
-                    continue;
-                }
-            }
-
-            pid_t pid = fork();
-            if (pid == 0) {
-                // Restaurar señales en procesos hijos
-                signal(SIGINT, SIG_DFL);
-                signal(SIGQUIT, SIG_DFL);
-
-                if (input_fd != -1) {
-                    dup2(input_fd, STDIN_FILENO);
-                    close(input_fd);
-                }
-                if (output_fd != -1) {
-                    dup2(output_fd, STDOUT_FILENO);
-                    close(output_fd);
-                }
-                if (error_fd != -1) {
-                    dup2(error_fd, STDERR_FILENO);
-                    close(error_fd);
-                }
-
-                tcommand *cmd = &line->commands[0];
-                execv(cmd->filename, cmd->argv);
-                fprintf(stderr, "Error al ejecutar el comando %s\n", cmd->filename);
-                exit(1);
-            } else if (pid > 0) {
-                if (line->background) {
-                    // Realocar dinámicamente la lista de trabajos
-                    jobs = realloc(jobs, (job_count + 1) * sizeof(job_t));
-                    if (jobs == NULL) {
-                        fprintf(stderr, "Error al realocar memoria para trabajos\n");
-                        exit(1);
+            if (strcmp(command, "cd") == 0 || strncmp(command, "cd ", 3) == 0) {
+                char *dir = strtok(command + 3, "\n");
+                if (dir == NULL) dir = getenv("HOME");
+                if (chdir(dir) == -1) fprintf(stderr, "Error al cambiar de directorio\n");
+            } else if (strcmp(command, "jobs") == 0) {
+                for (int i = 0; i < job_count; i++) {
+                    if (jobs[i].active) {
+                        printf("[%d]+ %-7s %s\n", jobs[i].id, jobs[i].status, jobs[i].command);
                     }
-                    jobs[job_count].id = next_job_id++;
-                    jobs[job_count].pid = pid;
-                    jobs[job_count].active = 1;
-                    strncpy(jobs[job_count].command, line->commands[0].filename, sizeof(jobs[job_count].command));
-                    strncpy(jobs[job_count].status, "Running", sizeof(jobs[job_count].status));
-                    job_count++;
-                } else {
-                    waitpid(pid, NULL, 0);
                 }
+            } else if (strncmp(command, "fg", 2) == 0) {
+                char *index = strtok(command + 3, "\n");
+                fg(index);
             } else {
-                fprintf(stderr, "Error al crear el proceso hijo\n");
+                int input_fd = -1, output_fd = -1, error_fd = -1;
+
+                // **Redirección de entrada**
+                if (line->redirect_input) {
+                    input_fd = open(line->redirect_input, O_RDONLY);
+                    if (input_fd == -1) {
+                        fprintf(stderr, "Error al abrir el archivo de entrada: %s\n", line->redirect_input);
+                        command = strtok(NULL, ";\n");
+                        continue;
+                    }
+                }
+
+                // **Redirección de salida**
+                if (line->redirect_output) {
+                    output_fd = open(line->redirect_output, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                    if (output_fd == -1) {
+                        fprintf(stderr, "Error al abrir el archivo de salida: %s\n", line->redirect_output);
+                        if (input_fd != -1) close(input_fd);
+                        command = strtok(NULL, ";\n");
+                        continue;
+                    }
+                }
+
+                // **Redirección de error**
+                if (line->redirect_error) {
+                    error_fd = open(line->redirect_error, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                    if (error_fd == -1) {
+                        fprintf(stderr, "Error al abrir el archivo de error: %s\n", line->redirect_error);
+                        if (input_fd != -1) close(input_fd);
+                        if (output_fd != -1) close(output_fd);
+                        command = strtok(NULL, ";\n");
+                        continue;
+                    }
+                }
+
+                pid_t pid = fork();
+                if (pid == 0) {
+                    // Restaurar señales en procesos hijos
+                    signal(SIGINT, SIG_DFL);
+                    signal(SIGQUIT, SIG_DFL);
+
+                    if (input_fd != -1) {
+                        dup2(input_fd, STDIN_FILENO);
+                        close(input_fd);
+                    }
+                    if (output_fd != -1) {
+                        dup2(output_fd, STDOUT_FILENO);
+                        close(output_fd);
+                    }
+                    if (error_fd != -1) {
+                        dup2(error_fd, STDERR_FILENO);
+                        close(error_fd);
+                    }
+
+                    tcommand *cmd = &line->commands[0];
+                    execv(cmd->filename, cmd->argv);
+                    fprintf(stderr, "Error al ejecutar el comando %s\n", cmd->filename);
+                    exit(1);
+                } else if (pid > 0) {
+                    if (line->background) {
+                        // Realocar dinámicamente la lista de trabajos
+                        jobs = realloc(jobs, (job_count + 1) * sizeof(job_t));
+                        if (jobs == NULL) {
+                            fprintf(stderr, "Error al realocar memoria para trabajos\n");
+                            exit(1);
+                        }
+                        jobs[job_count].id = next_job_id++;
+                        jobs[job_count].pid = pid;
+                        jobs[job_count].active = 1;
+                        strncpy(jobs[job_count].command, line->commands[0].filename, sizeof(jobs[job_count].command));
+                        strncpy(jobs[job_count].status, "Running", sizeof(jobs[job_count].status));
+                        job_count++;
+                    } else {
+                        waitpid(pid, NULL, 0);
+                    }
+                } else {
+                    fprintf(stderr, "Error al crear el proceso hijo\n");
+                }
+
+                if (input_fd != -1) close(input_fd);
+                if (output_fd != -1) close(output_fd);
+                if (error_fd != -1) close(error_fd);
             }
 
-            if (input_fd != -1) close(input_fd);
-            if (output_fd != -1) close(output_fd);
-            if (error_fd != -1) close(error_fd);
+            command = strtok(NULL, ";\n");
         }
     }
 
